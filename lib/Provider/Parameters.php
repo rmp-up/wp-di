ed6518a3ad458f909e062a77d95b2968572ed64a
Pretzlaw <mail+99@mike-pretzlaw.de>
2019-04-27 22:32:19 +0200

    Provider: Use primitives as arguments

---
+++ lib/Provider/Parameters.php
@@ -0,0 +1,62 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Parameters.php
+ *
+ * LICENSE: This source file is created by the company around Mike Pretzlaw
+ * located in Germany also known as rmp-up. All its contents are proprietary
+ * and under german copyright law. Consider this file as closed source and/or
+ * without the permission to reuse or modify its contents.
+ * This license is available through the world-wide-web at the following URI:
+ * https://mike-pretzlaw.de/license-generic.txt . If you did not receive a copy
+ * of the license and are unable to obtain it through the web, please send a
+ * note to mail@mike-pretzlaw.de so we can mail you a copy.
+ *
+ * @package    wp-di
+ * @copyright  2019 Mike Pretzlaw
+ * @license    https://mike-pretzlaw.de/license-generic.txt
+ * @link       https://project.mike-pretzlaw.de/wp-di
+ * @since      2019-04-27
+ */
+
+declare(strict_types=1);
+
+namespace RmpUp\WpDi\Provider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Parameters
+ *
+ * @copyright  2019 Mike Pretzlaw (https://mike-pretzlaw.de)
+ * @since      2019-04-27
+ */
+class Parameters implements ServiceProviderInterface
+{
+    /**
+     * @var array
+     */
+    private $parameters;
+
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple A container instance
+     */
+    public function register(Container $pimple)
+    {
+        foreach ($this->parameters as $parameterName => $value) {
+            $pimple[$parameterName] = $value;
+        }
+    }
+}
---
+++ lib/Test/Provider/Parameters/KeepStringsTest.php
@@ -0,0 +1,75 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * KeepStringsTest.php
+ *
+ * LICENSE: This source file is created by the company around Mike Pretzlaw
+ * located in Germany also known as rmp-up. All its contents are proprietary
+ * and under german copyright law. Consider this file as closed source and/or
+ * without the permission to reuse or modify its contents.
+ * This license is available through the world-wide-web at the following URI:
+ * https://mike-pretzlaw.de/license-generic.txt . If you did not receive a copy
+ * of the license and are unable to obtain it through the web, please send a
+ * note to mail@mike-pretzlaw.de so we can mail you a copy.
+ *
+ * @package    wp-di
+ * @copyright  2019 Mike Pretzlaw
+ * @license    https://mike-pretzlaw.de/license-generic.txt
+ * @link       https://project.mike-pretzlaw.de/wp-di
+ * @since      2019-04-27
+ */
+
+declare(strict_types=1);
+
+namespace RmpUp\WpDi\Test\Provider\Parameters;
+
+use RmpUp\WpDi\Provider\Parameters;
+use RmpUp\WpDi\Provider\Services;
+use RmpUp\WpDi\Test\AbstractTestCase;
+use RmpUp\WpDi\Test\Mirror;
+
+/**
+ * KeepStringsTest
+ *
+ * @copyright  2019 Mike Pretzlaw (https://mike-pretzlaw.de)
+ * @since      2019-04-27
+ */
+class KeepStringsTest extends AbstractTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->pimple->register(new Parameters([
+            'foo' => 'bar',
+            'bar' => 'baz',
+            'qux' => 'quxx',
+        ]));
+    }
+
+    public function testStringsStayAsTheyAre()
+    {
+        static::assertEquals('bar', $this->container->get('foo'));
+        static::assertEquals('baz', $this->container->get('bar'));
+        static::assertEquals('quxx', $this->container->get('qux'));
+    }
+
+    public function testStringsUsedAsArgument()
+    {
+        $this->pimple->register(new Services([
+            Mirror::class => [
+                Services::CLASS_NAME => Mirror::class,
+                Services::ARGUMENTS => [
+                    'foo',
+                    'qux',
+                ]
+            ]
+        ]));
+
+        /** @var Mirror $mirror */
+        $mirror = $this->container->get(Mirror::class);
+
+        static::assertEquals(['bar', 'quxx'], $mirror->getConstructorArgs());
+    }
+}
