ca06302842a6b441cabb6dcc4a5eba11cc166cdb
Pretzlaw <mail+99@mike-pretzlaw.de>
2019-05-29 08:39:48 +0200

    qa: Check if actions have been registered

---
+++ lib/Test/Provider/WpActions/RegistersActionTest.php
@@ -0,0 +1,80 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * ClosuresTest.php
+ *
+ * LICENSE: This source file is created by the company around Mike Pretzlaw
+ * located in Germany also known as rmp-up. All its contents are proprietary
+ * and under german copyright law. Consider this file as closed source and/or
+ * without the permission to reuse or modify its contents.
+ * This license is available through the world-wide-web at the following URI:
+ * https://mike-pretzlaw.de/license-generic.txt . If you did not receive a copy
+ * of the license and are unable to obtain it through the web, please send a
+ * note to mail@mike-pretzlaw.de so we can mail you a copy.
+ *
+ * @package    wp-di
+ * @copyright  2019 Mike Pretzlaw
+ * @license    https://mike-pretzlaw.de/license-generic.txt
+ * @link       https://project.mike-pretzlaw.de/wp-di
+ * @since      2019-05-28
+ */
+
+declare(strict_types=1);
+
+namespace RmpUp\WpDi\Test\Provider\WpActions;
+
+use RmpUp\WpDi\Provider\WpActions;
+use RmpUp\WpDi\Test\Mirror;
+
+/**
+ * ClosuresTest
+ *
+ * @copyright  2019 Mike Pretzlaw (https://mike-pretzlaw.de)
+ * @since      2019-05-28
+ */
+class RegistersActionTest extends AbstractWpActionsTest
+{
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|WpActions
+     */
+    private $provider;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->pimple[Mirror::class] = new Mirror();
+
+        $services = $this->sanitizer->sanitize([
+            'foo_event' => [
+                [
+                    WpActions::SERVICE => Mirror::class,
+                    WpActions::PRIORITY => 1337,
+                    WpActions::ARG_COUNT => 42,
+                ],
+            ]
+        ]);
+
+        $this->provider = $this->getMockBuilder(WpActions::class)
+            ->setConstructorArgs([$services])
+            ->setMethods(['registerAction'])
+            ->enableProxyingToOriginalMethods()
+            ->getMock();
+    }
+
+    public function testClosureIsRegistered()
+    {
+        $this->provider->expects($this->once())
+            ->method('registerAction')
+            ->with(
+                'foo_event',
+                $this->container,
+                Mirror::class,
+                1337,
+                42
+            );
+
+        $this->provider->register($this->pimple);
+    }
+}
