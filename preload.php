641ae3b2784523a523c4f445f77443884e1530cf
rmp-up <mail@rmp-up.de>
2020-10-28 12:20:51 +0100

    performance: preload.php for faster loading
    Initiating by finding files via composer
    or loading them in each request
    can take some time.
    The "preload.php" can avoid this overhead.

---
+++ preload.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'lib/Compiler/CompilerInterface.php';
+require_once 'lib/Compiler/Filter.php';
+require_once 'lib/Compiler/MetaBox.php';
+require_once 'lib/Compiler/PostType.php';
+require_once 'lib/Compiler/Shortcode.php';
+require_once 'lib/Compiler/Widgets.php';
+require_once 'lib/Compiler/WpCli.php';
+require_once 'lib/Compiler/Yaml/Join.php';
+require_once 'lib/Compiler/Yaml/Translate.php';
+require_once 'lib/Compiler/Yaml/YamlCompiler.php';
+require_once 'lib/Provider/Parameters.php';
+require_once 'lib/Provider/ProviderNode.php';
+require_once 'lib/Provider/ProviderNodeTrait.php';
+require_once 'lib/Provider/Services.php';
+require_once 'lib/Provider/WordPress/Options.php';
+require_once 'lib/Provider/WordPress/Templates.php';
